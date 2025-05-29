57d110c3da3906b449f978c26b8eb739099d2fac
carloslemus28 <163664063+carloslemus28@users.noreply.github.com>
2025-05-29 15:31:40 -0600

    Update ruletaDB.sql

--- ruletaDB.sql
+++ ruletaDB.sql
@@ -1,29 +1,27 @@
-drop database ruletaDB;
 create database ruletaDB;
+
 use ruletaDB;
--- Tabla de Usuarios
+
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     correo VARCHAR(100) UNIQUE NOT NULL,
-    contrasena VARCHAR(255) NOT NULL, -- Se almacenará encriptada
-    coins INT DEFAULT 100 NOT NULL, -- Cada jugador inicia con 100 COINS
+    contrasena VARCHAR(255) NOT NULL,
+    coins INT DEFAULT 100 NOT NULL,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Tabla de Historial de Apuestas
 CREATE TABLE historial_apuestas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
-    tipo_apuesta ENUM('color', 'numero') NOT NULL, -- Tipo de apuesta
-    valor_apostado VARCHAR(10) NOT NULL, -- Puede ser 'rojo', 'negro', 'verde' o un número entre 1 y 36
-    monto_apostado INT NOT NULL, -- Cantidad de COINS apostadas
+    tipo_apuesta ENUM('color', 'numero') NOT NULL,
+    valor_apostado VARCHAR(10) NOT NULL,
+    monto_apostado INT NOT NULL,
     resultado ENUM('ganado', 'perdido') NOT NULL,
-    coins_ganados INT DEFAULT 0, -- Cantidad de COINS ganadas o 0 si perdió
-    numero_ganador INT NOT NULL, -- Número en el que cayó la ruleta
-    color_ganador ENUM('rojo', 'negro', 'verde') NOT NULL, -- Color del número ganador
+    coins_ganados INT DEFAULT 0,
+    numero_ganador INT NOT NULL,
+    color_ganador ENUM('rojo', 'negro', 'verde') NOT NULL,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
-select * from usuarios
